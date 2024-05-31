bf7ea59ba433f874c2e0b26596ab14ef13b4f5a1
KagurazakaNyaa <dev@kagurazakanyaa.com>
2024-05-31 20:04:13 +0800

    fix password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,16 +9,16 @@ if [[ -n $INIT_BACKUP ]] && [[ $INIT_BACKUP == "true" ]]; then
     extra_opts="-initbackup"
 fi
 if [[ -n $BACKUP_INTERVAL_MINUTES ]]; then
-    extra_opts="$extra_opts -backupinterval ${BACKUP_INTERVAL_MINUTES}"
+    extra_opts="$extra_opts -backupinterval=${BACKUP_INTERVAL_MINUTES}"
 fi
 if [[ -n $SAVED_DIR_SUFFIX ]]; then
-    extra_opts="$extra_opts -saveddirsuffix ${SAVED_DIR_SUFFIX}"
+    extra_opts="$extra_opts -saveddirsuffix=\"${SAVED_DIR_SUFFIX}\""
 fi
 if [[ -n $ADMIN_PASSWORD ]]; then
-    extra_opts="$extra_opts -adminpsw ${ADMIN_PASSWORD}"
+    extra_opts="$extra_opts -adminpsw=\"${ADMIN_PASSWORD}\""
 fi
 if [[ -n $SERVER_PASSWORD ]]; then
-    extra_opts="$extra_opts -PSW ${SERVER_PASSWORD}"
+    extra_opts="$extra_opts -PSW=\"${SERVER_PASSWORD}\""
 fi
 
 if [ $# -eq 0 ]; then
@@ -26,7 +26,7 @@ if [ $# -eq 0 ]; then
     proc_serial=1
     while [ $proc_result == 128 ]; do
         /home/steam/steamcmd/steamcmd.sh +login anonymous +quit
-        ./WSServer.sh "${LEVEL_NAME}" -server -SLIENT -log -UTF8Output -SteamServerName="${SERVER_NAME}" -MaxPlayers="${MAX_PLAYERS}" -backup="${BACKUP_SYNC_INTERVAL_SECONDS}" -saving="${SAVING_SYNC_INTERVAL_SECONDS}" -MULTIHOME=0.0.0.0 -Port="${GAME_PORT}" -QueryPort="${QUERY_PORT}" -EchoPort="${ECHO_PORT}" -online=Steam -forcepassthrough "${extra_opts}"
+        ./WSServer.sh "${LEVEL_NAME}" -server -SLIENT -log -UTF8Output -SteamServerName=\""${SERVER_NAME}"\" -MaxPlayers="${MAX_PLAYERS}" -backup="${BACKUP_SYNC_INTERVAL_SECONDS}" -saving="${SAVING_SYNC_INTERVAL_SECONDS}" -MULTIHOME=0.0.0.0 -Port="${GAME_PORT}" -QueryPort="${QUERY_PORT}" -EchoPort="${ECHO_PORT}" -online=Steam -forcepassthrough "${extra_opts}"
         proc_result=$?
         echo $proc_result
         proc_serial=$((proc_serial + 1))
