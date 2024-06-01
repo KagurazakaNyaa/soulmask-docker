2511d6f0787d14d599825387844063c0162e6742
KagurazakaNyaa <dev@kagurazakanyaa.com>
2024-06-01 20:08:18 +0800

    fix Too many quotation marks in the start parameter #1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,27 +11,27 @@ if [[ -n $FORCE_UPDATE ]] && [[ $FORCE_UPDATE == "true" ]]; then
     /home/steam/steamcmd/steamcmd.sh +force_install_dir "/opt/soulmask" +login anonymous +app_update 3017300 validate +quit
 fi
 
-extra_opts=""
+extra_opts=()
 if [[ -n $INIT_BACKUP ]] && [[ $INIT_BACKUP == "true" ]]; then
-    extra_opts="-initbackup"
+    extra_opts+=("-initbackup")
 fi
 if [[ -n $BACKUP_INTERVAL_MINUTES ]]; then
-    extra_opts="$extra_opts -backupinterval=${BACKUP_INTERVAL_MINUTES}"
+    extra_opts+=("-backupinterval=${BACKUP_INTERVAL_MINUTES}")
 fi
 if [[ -n $SAVED_DIR_SUFFIX ]]; then
-    extra_opts="$extra_opts -saveddirsuffix=\"${SAVED_DIR_SUFFIX}\""
+    extra_opts+=("-saveddirsuffix=\"${SAVED_DIR_SUFFIX}\"")
 fi
 if [[ -n $ADMIN_PASSWORD ]]; then
-    extra_opts="$extra_opts -adminpsw=\"${ADMIN_PASSWORD}\""
+    extra_opts+=("-adminpsw=\"${ADMIN_PASSWORD}\"")
 fi
 if [[ -n $SERVER_PASSWORD ]]; then
-    extra_opts="$extra_opts -PSW=\"${SERVER_PASSWORD}\""
+    extra_opts+=("-PSW=\"${SERVER_PASSWORD}\"")
 fi
 if [[ -n $GAME_MODE ]]; then
     if [[ $GAME_MODE == "pve" ]]; then
-        extra_opts="$extra_opts -pve"
+        extra_opts+=("-pve")
     elif [[ $GAME_MODE == "pvp" ]]; then
-        extra_opts="$extra_opts -pvp"
+        extra_opts+=("-pvp")
     fi
 fi
 
@@ -41,7 +41,7 @@ if [ $# -eq 0 ]; then
     proc_serial=1
     while [ $proc_result == 128 ]; do
         /home/steam/steamcmd/steamcmd.sh +login anonymous +quit
-        ./WSServer.sh "${LEVEL_NAME}" -server -SLIENT -log -UTF8Output -SteamServerName=\""${SERVER_NAME}"\" -MaxPlayers="${MAX_PLAYERS}" -backup="${BACKUP_SYNC_INTERVAL_SECONDS}" -saving="${SAVING_SYNC_INTERVAL_SECONDS}" -MULTIHOME=0.0.0.0 -Port="${GAME_PORT}" -QueryPort="${QUERY_PORT}" -EchoPort="${ECHO_PORT}" -online=Steam -forcepassthrough "${extra_opts}" &
+        ./WSServer.sh "${LEVEL_NAME}" -server -SLIENT -log -UTF8Output -SteamServerName=\""${SERVER_NAME}"\" -MaxPlayers="${MAX_PLAYERS}" -backup="${BACKUP_SYNC_INTERVAL_SECONDS}" -saving="${SAVING_SYNC_INTERVAL_SECONDS}" -MULTIHOME=0.0.0.0 -Port="${GAME_PORT}" -QueryPort="${QUERY_PORT}" -EchoPort="${ECHO_PORT}" -online=Steam -forcepassthrough "${extra_opts[@]}" &
         proc_result=$?
         pid=$!
         echo pid=$pid result=$proc_result
