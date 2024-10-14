f46454505557bb9bcf825bd5fbb14f5a7fe8fb96
神楽坂·喵 <dev@kagurazakanyaa.com>
2024-10-14 18:19:12 +0800

    fix null version

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 version=$(curl --silent "https://api.steamcmd.net/v1/info/3017300" | jq '.data."3017300".depots.branches.public.buildid' -r)
 currentversion=$(cat currentversion)
 echo "currentversion:$currentversion version:$version"
+if [[ -z "${version}" ]]; then
+    exit
+fi
 echo "$version" >currentversion
 if [[ "$currentversion" == "$version" ]]; then
     exit
